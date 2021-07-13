8026dec9c8e7c3c33ee411f8ccaf50db4089b949
Andrey Solera <andrey.valverde.solera@gmail.com>
2021-07-13 07:49:07 -0600

    Initial commit

--- STARTERS/redux-saga-starter/src/redux/store.js
+++ STARTERS/redux-saga-starter/src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./rootReducer";
 import ReduxThunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga"
 
 const middleware = [ReduxThunk];
 const store = createStore(
